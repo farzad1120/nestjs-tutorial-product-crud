681c81b904a115d72b2dace6ce5636da35f75247
Farzad Zarasvand <dev.farzadz@gmail.com>
2020-09-07 21:54:13 +0430

    read port from environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port || 3000, () =>
+    Logger.debug(`App has been started at port ${port}.`, 'Bootstrap'),
+  );
 }
 bootstrap();
